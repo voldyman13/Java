da9b706fc3528c214a975f86e35944df7d03ce9d
Vladimir <voldyman@gmail.com>
2020-03-08 00:03:36 +0200

    add method getRandomNumber

--- src/main/java/UniqueMembersOfRandomArray.java
+++ src/main/java/UniqueMembersOfRandomArray.java
@@ -2,11 +2,10 @@ import java.util.Random;
 
 public class UniqueMembersOfRandomArray {
     public static void main(String[]args){
-        int length = 50; // length of array
-        int bound = 20;  // bound of random members
-        int[] array = new int[length];
-        getRandomArray(array, bound);
-        getUniqueMembers(array);
+        int length = 31; // length of array
+        int bound = 1;  // bound of random members
+//        getUniqueMembers(getRandomArray(length, bound));
+        getRandomNumber(bound, length);
     }
 
     public static void getUniqueMembers(int[] array) {
@@ -14,11 +13,9 @@ public class UniqueMembersOfRandomArray {
         for (int i=0; i<array.length; i++) {
             boolean flag = true;
             for (int j=0; j<array.length; j++){
-                if (j!=i) {
-                    if (array[j] == array[i]) {
+                if (j!=i && array[j] == array[i]) {
                         flag = false;
                         break;
-                    }
                 }
             }
             if(flag){
@@ -28,7 +25,8 @@ public class UniqueMembersOfRandomArray {
         System.out.println("]");
     }
 
-    public static void getRandomArray(int[] array, int bound) {
+    public static int[] getRandomArray(int length, int bound) {
+        int[] array = new int[length];
         Random rd = new Random();
         int len = array.length;
         System.out.print("Array: [ ");
@@ -37,5 +35,16 @@ public class UniqueMembersOfRandomArray {
             System.out.print(array[i]+" ");
         }
         System.out.println("]");
+        return array;
     }
+    public static int getRandomNumber(int first, int last) {
+        Random rd = new Random();
+        int number = first-1;
+        while(number<=first-1) {
+            number = rd.nextInt(last-first-1);
+        }
+        System.out.println(number);
+        return number;
+    }
+
 }
